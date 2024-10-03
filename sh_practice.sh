1f846ddd0b1c9b6f7a3f08736c2b4861baded3f7
Zairen <Zairen.Z@foxmail.com>
2024-10-03 10:07:07 -0700

    add sh practice

---
+++ sh_practice.sh
@@ -0,0 +1,7 @@
+echo "Use ""man echo"" "
+sleep 5 && echo "Hello, World!"
+touch foo && mv foo bar && cp bar baz
+ls -lF b*
+rm bar baz
+echo "Hello, World!" > hello.txt
+echo "Welcome to $USER's computer"
